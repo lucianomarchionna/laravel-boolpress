4521397467c14085b4d1cac2bc8cb8fc56838462
Luciano <lucco6823@gmail.com>
2021-11-07 12:02:19 +0100

    Creazione Posts

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Scrivi un nuovo Post</h1>
+                <form action="{{ route('admin.posts.store') }}">
+                    @csrf
+                    @method('POST')
+                    <div class="form-group">
+                        <label for="title">Titolo</label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Contenuto</label>
+                        <textarea name="content" id="content" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success">Crea il post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
