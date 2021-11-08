99c163b979100ae575f1ec0301cd487a205f06fe
Luciano <lucco6823@gmail.com>
2021-11-08 16:04:19 +0100

    Aggiunte Categories

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Category;
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['HTML', 'CSS', 'JS', 'PHP', 'LAVAREL', 'VUE CLI'];
+
+        
+
+        for($i = 0; $i < count($categories); $i++){
+            $new_category = new Category();
+            $new_category->name = $categories[$i];
+            $new_category->slug = Str::slug($categories[$i], '-');
+            $new_category->save();
+        }
+    }
+}
