f174541700725f7f55959f1830aa090560c4e32a
Luciano <lucco6823@gmail.com>
2021-11-08 18:06:43 +0100

    Aggiunta

--- database/migrations/2021_11_08_150332_add_foreign_key_posts_table.php
+++ database/migrations/2021_11_08_150332_add_foreign_key_posts_table.php
@@ -15,7 +15,7 @@ class AddForeignKeyPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->unsignedBigInteger('category_id')->nullable()->after('slug');
-            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
@@ -27,6 +27,7 @@ class AddForeignKeyPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_category_id_foreign');
             $table->dropColumn('category_id');
         });
     }
